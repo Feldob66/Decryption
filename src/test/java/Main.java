f32d6b06e144f537cdbda3b4eec06702b561c21c
szabi125 <bertat06@gmail.com>
2025-05-17 13:42:11 +0200

    Test main
    Test Main Importok

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,13 @@
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import com.example.decryption.*;
 
 public class Main {
+
+    @Test
+    public void Testmethod1(){
+
+    }
 
 
 }
